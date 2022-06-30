8846637ffde5d598f783a111cf4bfc22c2a5e025
konisz <kmateusz2012@gmail.com>
2022-06-30 20:20:05 +0200

    All digits implemented and tested.

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #define dataPin 14      // D5 -> SERIAL INPUT
 #define clockPin 13     // D7 -> SR CLOCK INPUT
 
-int digits[] = {0xC0 ,0xF9 ,0xA4 ,0xB0 ,0x99, 0x92, 0x82, 0xF8, 0x80, 0x90};
+int digits[] = {0xC0 ,0xF9 ,0xA4 ,0xB0 ,0x99, 0x92, 0x82, 0xF8, 0x80, 0x90, 0xFF};
 
 void SET_ADDR(int num) {
     digitalWrite(latchPin, LOW);
@@ -13,17 +13,31 @@ void SET_ADDR(int num) {
     digitalWrite(latchPin, HIGH);
 }
 
+void digitOutput(int d1,int d2, int d3, int d4, int d5, int d6){
+    digitalWrite(latchPin,LOW);
+    shiftOut(dataPin, clockPin, MSBFIRST, digits[d6]);
+    shiftOut(dataPin, clockPin, MSBFIRST, digits[d5]);
+    shiftOut(dataPin, clockPin, MSBFIRST, digits[d4]);
+    shiftOut(dataPin, clockPin, MSBFIRST, digits[d3]);
+    shiftOut(dataPin, clockPin, MSBFIRST, digits[d2]);
+    shiftOut(dataPin, clockPin, MSBFIRST, digits[d1]);
+    digitalWrite(latchPin,HIGH);
+}
+
 void setup() {
     pinMode(latchPin, OUTPUT);
     pinMode(clockPin, OUTPUT);
     pinMode(dataPin, OUTPUT);
+
+    digitOutput(1,2,10,10,3,2);
 }
 
 void loop(){
-
-    for(int i = 0; i <= 9; i++ )
+/*
+    for(int i = 0; i <= 10; i++ )
     {
         SET_ADDR(digits[i]);
         delay(300);
     }
+    */
 }
