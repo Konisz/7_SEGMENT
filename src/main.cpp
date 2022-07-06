73efcd2581b6d70cbe62fea0c173de2a7c5cb2e0
konisz <kmateusz2012@gmail.com>
2022-07-06 02:24:33 +0200

    WiFi connection and MYSQL data sending via PHP

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,6 @@
 #include <DallasTemperature.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
-#include <WiFiClient.h>
 #include "credentials.h"
 
 //#PORTS
@@ -17,7 +16,7 @@ OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature ds(&oneWire);
 
 //#DIGITS TABLE
-int digits[] = {0xC0 ,0xF9 ,0xA4 ,0xB0 ,0x99, 0x92, 0x82, 0xF8, 0x80, 0x90, 0xFF};
+int digits[] = {0xC0 ,0xF9 ,0xA4 ,0xB0 ,0x99, 0x92, 0x82, 0xF8, 0x80, 0x90, 0xFF,0xBF, 0xAB};
 
 //#DATA STORAGE
 int last_temp1;
@@ -26,13 +25,13 @@ int chck_temp1;
 int chck_temp2;
 
 //#DELAYS
-unsigned  millisTimer;
-unsigned rd_temp;
-unsigned rd_screen;
-/*
+//unsigned millisTimer;
+//unsigned refresh_time_h;
+//unsigned refresh_time = 6000UL;
+
 WiFiClient client;
-int status = WL_IDLE_STATUS;
-*/
+String apiKeyValue = "tPmAT5Ab3j7F9";
+
 //#FUNC SENDING DATA TO SHIFT REGISTERS
 void digitOutput(int d1,int d2, int d3, int d4, int d5, int d6){
     digitalWrite(latchPin,LOW);
@@ -46,7 +45,6 @@ void digitOutput(int d1,int d2, int d3, int d4, int d5, int d6){
 }
 
 void readTemperatureDEBUG(){
-    if(millisTimer - rd_temp >= 10000UL){
         Serial.print("# Requesting temperatures...");
         ds.requestTemperatures();
         Serial.println("DONE");
@@ -62,24 +60,20 @@ void readTemperatureDEBUG(){
         Serial.print("# Reading from sensor #1 (variable): ");
         Serial.println(last_temp2);
         Serial.println("###################");
-        rd_temp = millisTimer;
-    }
 }
 
 void readTemperature(){
-    if(millisTimer - rd_temp >= 10000UL){
+
         ds.requestTemperatures();
         if(ds.getTempCByIndex(0) > 10 && ds.getTempCByIndex(0) < 40)
         last_temp1 = ds.getTempCByIndex(0);
         if(ds.getTempCByIndex(1) > 10 && ds.getTempCByIndex(1) < 40)
         last_temp2 = ds.getTempCByIndex(1);
-        rd_temp = millisTimer;
-    }
+
 }
 
 void refreshTemperatureOnDisplay(){
-    if(millisTimer - rd_screen >= 10000UL)
-    {
+
         if((chck_temp1 != last_temp1) || (chck_temp2 != last_temp2)){
             int d1 = (last_temp1 / 10) % 10;
             int d2 = last_temp1 % 10;
@@ -89,29 +83,62 @@ void refreshTemperatureOnDisplay(){
             chck_temp1 = last_temp1;
             chck_temp2 = last_temp2;
         }
-        rd_screen = millisTimer;
+
+}
+
+void sendDataToDatabase(){
+
+        if(WiFi.status()== WL_CONNECTED){
+            WiFiClient client;
+            HTTPClient http;
+            http.begin(client, ESP_DATA_PICK);
+            http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+            String httpRequestData = "api_key=" + apiKeyValue + "&sensor0=" + last_temp1 + "&sensor1=" + last_temp2 + "";
+            Serial.print("httpRequestData: ");
+            Serial.println(httpRequestData);
+            int httpResponseCode = http.POST(httpRequestData);
+            if (httpResponseCode>0) {
+                 Serial.print("HTTP Response code: ");
+                Serial.println(httpResponseCode);
+            }
+            else {
+                Serial.print("Error code: ");
+                Serial.println(httpResponseCode);
+            }
+        http.end();
     }
+    else {
+    Serial.println("WiFi Disconnected");
+    }
+
 }
 
 void setup() {
+    //Serial.begin(115200);
     ds.begin();
-    Serial.begin(9600);
     pinMode(latchPin, OUTPUT);
     pinMode(clockPin, OUTPUT);
     pinMode(dataPin, OUTPUT);
-    /*
-    while(status != WL_CONNECTED)
+    digitOutput(11,11,0,12,11,11);
+
+    WiFi.begin(WIFI_SSID,WIFI_PASSW);
+    while(WiFi.status() != WL_CONNECTED)
     {
-        status = WiFi.begin(WIFI_SSID,WIFI_PASSW);
-        delay(5000);
+        Serial.print(".");
+        delay(500);
     }
     Serial.println(WiFi.localIP());
-     */
+     
 }
 
 void loop(){
-    millisTimer = millis();
+    //millisTimer = millis();
+    //if(millisTimer - refresh_time_h >= refresh_time){
     readTemperature();
     refreshTemperatureOnDisplay();
+    sendDataToDatabase();
+    delay(10000);
+    //}
+    //refresh_time_h = millisTimer;
 }
 
